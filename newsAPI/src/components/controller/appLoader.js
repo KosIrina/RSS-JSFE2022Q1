69e6e302060b70656b7b8189424ca06ab986aa54
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-06-30 05:17:33 +0300

    feat: add apiKey to appLoader.js

--- newsAPI/src/components/controller/appLoader.js
+++ newsAPI/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'a4a0558513fe4a638309e3db3a74de68',
         });
     }
 }
