30cc3e0ebdfef191c2f9da9abba0e5f1b811688e
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-07-01 10:44:42 +0300

    feat: add main types and interfaces

---
+++ newsAPI/src/types/types.ts
@@ -0,0 +1,33 @@
+export interface Everything {
+    status: string;
+    totalResults: number;
+    articles: Article[];
+}
+
+export interface Article {
+    source: Pick<Source, 'id' | 'name'>;
+    author: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string;
+    publishedAt: string;
+    content: string;
+}
+
+export interface AllSources {
+    status: string;
+    sources: Source[];
+}
+
+export interface Source {
+    id: string | null;
+    name: string;
+    description: string;
+    url: string;
+    category: string;
+    language: string;
+    country: string;
+}
+
+export type EndPoint = 'sources' | 'everything';
