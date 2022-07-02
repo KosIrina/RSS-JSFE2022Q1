4643d8ddc5dbb7d935d6ceb271131bdc8d7b1b38
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-07-02 09:37:00 +0300

    fix: adjust loader-file according to typescript

--- newsAPI/src/components/controller/loader.ts
+++ newsAPI/src/components/controller/loader.ts
@@ -1,45 +1,58 @@
+import type { EndPoint, VoidCallback } from '../../types/types';
+import { ResponseErrors } from '../../types/types';
+
 class Loader {
-    constructor(baseLink, options) {
+    readonly baseLink: string;
+    readonly options: { [key: string]: string };
+    constructor(baseLink: string, options: { [key: string]: string }) {
         this.baseLink = baseLink;
         this.options = options;
     }
 
-    getResp(
-        { endpoint, options = {} },
-        callback = () => {
+    protected getResp<T>(
+        { endpoint, options = {} }: { endpoint: EndPoint; options?: { [key: string]: string } },
+        callback: VoidCallback<T> = () => {
             console.error('No callback for GET response');
         }
-    ) {
+    ): void {
         this.load('GET', endpoint, callback, options);
     }
 
-    errorHandler(res) {
+    private errorHandler(res: Response): Response {
         if (!res.ok) {
-            if (res.status === 401 || res.status === 404)
+            if (res.status === ResponseErrors.apiKeyMissing || res.status === ResponseErrors.notFound)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
             throw Error(res.statusText);
         }
-
         return res;
     }
 
-    makeUrl(options, endpoint) {
-        const urlOptions = { ...this.options, ...options };
+    private makeUrl(endpoint: EndPoint, options?: { [key: string]: string }): string {
+        let urlOptions: { [key: string]: string };
+        if (options !== undefined) {
+            urlOptions = { ...this.options, ...options };
+        } else {
+            urlOptions = { ...this.options };
+        }
         let url = `${this.baseLink}${endpoint}?`;
 
         Object.keys(urlOptions).forEach((key) => {
             url += `${key}=${urlOptions[key]}&`;
         });
-
         return url.slice(0, -1);
     }
 
-    load(method, endpoint, callback, options = {}) {
-        fetch(this.makeUrl(options, endpoint), { method })
+    private load<T>(
+        method: string,
+        endpoint: EndPoint,
+        callback: VoidCallback<T>,
+        options: { [key: string]: string } = {}
+    ): void {
+        fetch(this.makeUrl(endpoint, options), { method })
             .then(this.errorHandler)
-            .then((res) => res.json())
-            .then((data) => callback(data))
-            .catch((err) => console.error(err));
+            .then((res: Response) => res.json())
+            .then((data: T) => callback(data))
+            .catch((err: Error) => console.error(err));
     }
 }
 
