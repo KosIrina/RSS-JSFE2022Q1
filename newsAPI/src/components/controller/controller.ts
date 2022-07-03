411076f73d582bf9bf147183c2feee5ac4a86479
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-07-03 10:33:54 +0300

    feat: add method for loading news on page load, add display of current source name

--- newsAPI/src/components/controller/controller.ts
+++ newsAPI/src/components/controller/controller.ts
@@ -11,12 +11,29 @@ class AppController extends AppLoader {
         );
     }
 
+    public getInitialNews(callback: VoidCallback<Everything>): void {
+        const sourceId = 'abc-news';
+        (<HTMLDivElement>document.querySelector('.sources')).setAttribute('data-source', sourceId);
+        (<HTMLDivElement>document.querySelector('.news__heading')).innerText = 'News from ABC News';
+
+        super.getResp<Everything>(
+            {
+                endpoint: 'everything',
+                options: {
+                    sources: sourceId,
+                },
+            },
+            callback
+        );
+    }
+
     public getNews(e: Event, callback: VoidCallback<Everything>): void {
         let target = e.target as HTMLElement;
         const newsContainer = e.currentTarget as HTMLElement;
 
         while (target !== newsContainer) {
             if (target.classList.contains('source__item')) {
+                (<HTMLDivElement>document.querySelector('.news__heading')).innerText = `News from ${target.innerText}`;
                 const sourceId = target.getAttribute('data-source-id') as string;
                 if (newsContainer.getAttribute('data-source') !== sourceId) {
                     newsContainer.setAttribute('data-source', sourceId);
