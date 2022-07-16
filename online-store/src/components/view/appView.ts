c002ac4a0716b6d212c05e90ff3591ea24b72a94
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-07-16 11:04:19 +0300

    feat: add class appview including also methods for tablet

---
+++ online-store/src/components/view/appView.ts
@@ -0,0 +1,34 @@
+import Books from './books/books';
+import Sliders from './sliders/sliders';
+import { ListOfBooks } from '../../types/types';
+
+export class AppView {
+  readonly books: Books;
+  readonly sliders: Sliders;
+  constructor() {
+    this.books = new Books();
+    this.sliders = new Sliders();
+  }
+
+  public drawBooks(data: ListOfBooks): void {
+    this.books.draw(data);
+  }
+
+  public drawSliders(): void {
+    this.sliders.create();
+  }
+
+  public openMobileFilters(): void {
+    (document.querySelector('.settings__filters') as HTMLElement).classList.add('open');
+    (document.querySelector('.settings__filters-close') as HTMLElement).classList.add('open');
+    (document.querySelector('body') as HTMLElement).classList.add('noscroll');
+  }
+
+  public closeMobileFilters(): void {
+    (document.querySelector('.settings__filters') as HTMLElement).classList.remove('open');
+    (document.querySelector('.settings__filters-close') as HTMLElement).classList.remove('open');
+    (document.querySelector('body') as HTMLElement).classList.remove('noscroll');
+  }
+}
+
+export default AppView;
