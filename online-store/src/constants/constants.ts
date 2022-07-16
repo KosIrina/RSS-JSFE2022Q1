93865fffb89f3e31db524366964749aab5c4f713
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-07-16 09:27:13 +0300

    feat: add some enums

---
+++ online-store/src/constants/constants.ts
@@ -0,0 +1,15 @@
+export enum Numbers {
+  zero = 0,
+  one = 1,
+  three = 3,
+}
+
+export enum YearsOfPublication {
+  from = 2011,
+  to = 2022,
+}
+
+export enum BookQuantity {
+  minimum = 1,
+  maximum = 9,
+}
