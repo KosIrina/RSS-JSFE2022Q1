17a54f80d96a195fc4233d6a577be170015948e8
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-08-07 15:32:24 +0300

    fix: add updating amount of cars on page when deleting car

--- async-race/src/components/garage/controller/garage.ts
+++ async-race/src/components/garage/controller/garage.ts
@@ -1,11 +1,15 @@
 import API from '../../../api';
-import { COLOR } from '../../../constants';
+import CommonController from '../../common/controller/common';
+import { COLOR, APP_TEXT_CONTENT } from '../../../constants';
 
 export default class GarageController {
   readonly api: API;
 
+  readonly appController: CommonController;
+
   constructor() {
     this.api = new API();
+    this.appController = new CommonController();
   }
 
   public editCar(name: string, color: string, id: number): void {
@@ -36,5 +40,6 @@ export default class GarageController {
   public removeCar(id: string): void {
     const carInGarage = document.querySelector(`.garage__car[data-car-id="${id}"]`) as HTMLElement;
     carInGarage.remove();
+    this.appController.updateTotalCars(APP_TEXT_CONTENT.garage);
   }
 }
