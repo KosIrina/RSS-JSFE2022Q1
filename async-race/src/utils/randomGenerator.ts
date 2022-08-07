7936eecad08a3d0b3398e37a18474af83d3ef7f8
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-08-07 14:54:37 +0300

    feat: add class with methods for random generation

---
+++ async-race/src/utils/randomGenerator.ts
@@ -0,0 +1,29 @@
+import { carBrands, carModels } from '../data/brands-and-models';
+import { Numbers, COLOR } from '../constants';
+import { ICarParameters } from '../types';
+
+export default class RandomGenerator {
+  private generateRandomCarName(): string {
+    return `${carBrands[Math.floor(Math.random() * carBrands.length)]} ${
+      carModels[Math.floor(Math.random() * carModels.length)]
+    }`;
+  }
+
+  private generateRandomCarColor(): string {
+    const possibleSymols = COLOR.possibleSymbols;
+    let randomColor = COLOR.hash;
+    for (let iteration = Numbers.zero; iteration < Numbers.six; iteration += 1) {
+      randomColor += possibleSymols[Math.floor(Math.random() * possibleSymols.length)];
+    }
+    return randomColor;
+  }
+
+  public generateRandomCars(): ICarParameters[] {
+    return new Array(Numbers.hundred).fill(Numbers.zero).map(
+      (): ICarParameters => ({
+        name: this.generateRandomCarName(),
+        color: this.generateRandomCarColor(),
+      })
+    );
+  }
+}
