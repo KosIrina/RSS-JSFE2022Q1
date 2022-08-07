d6ba6ee83f728282c24a05ce715c445d80705e3f
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-08-07 15:29:19 +0300

    feat: add method to update total cars on page

--- async-race/src/components/common/controller/common.ts
+++ async-race/src/components/common/controller/common.ts
@@ -1,12 +1,16 @@
 import API from '../../../api';
 import { Numbers, COLOR } from '../../../constants';
 import { ICarsData } from '../../../types';
+import CommonView from '../view/common';
 
 export default class CommonController {
   readonly api: API;
 
+  readonly appView: CommonView;
+
   constructor() {
     this.api = new API();
+    this.appView = new CommonView();
   }
 
   public getCurrentPage(pageName: string): number {
@@ -20,5 +24,13 @@ export default class CommonController {
     const currentPage: number = this.getCurrentPage(pageName);
     const nextPage: ICarsData = await this.api.garage.getCars(currentPage + Numbers.one);
     return nextPage;
+  }
+
+  public async updateTotalCars(pageName: string): Promise<void> {
+    const currentPage = this.getCurrentPage(pageName.toLowerCase());
+
+    const pageHeading = document.querySelector('.garage__heading') as HTMLElement;
+    const cars = await this.api.garage.getCars(currentPage);
+    pageHeading.innerHTML = `${this.appView.createViewName(pageName, cars.carsTotal).innerHTML}`;
   }
 }
