80ad8fb009a3f2a9b818cf0ca81d2bd814901a17
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-08-07 14:58:57 +0300

    refactor: replace getting current winners page code with common method

--- async-race/src/components/winners/controller/winners.ts
+++ async-race/src/components/winners/controller/winners.ts
@@ -1,23 +1,25 @@
 import API from '../../../api';
 import WinnersView from '../view/winners';
-import { Numbers, COLOR } from '../../../constants';
+import CommonController from '../../common/controller/common';
+import { APP_TEXT_CONTENT } from '../../../constants';
 
 export default class WinnersController {
   readonly api: API;
 
   readonly winners: WinnersView;
 
+  readonly appController: CommonController;
+
   constructor() {
     this.api = new API();
     this.winners = new WinnersView();
+    this.appController = new CommonController();
   }
 
   public async removeCar(id: string): Promise<void> {
     const carInWinners = document.querySelector(`.winners__row[data-car-id="${id}"]`);
     if (carInWinners) {
-      const pageHeader = (document.querySelector('.winners__page-number') as HTMLElement)
-        .textContent as string;
-      const currentPage: number = +pageHeader.split(COLOR.hash)[Numbers.one];
+      const currentPage = this.appController.getCurrentPage(APP_TEXT_CONTENT.winners.toLowerCase());
       await this.api.winners.deleteWinner(+id);
       const winners = document.querySelector('.main__winners') as HTMLElement;
       winners.innerHTML = '';
