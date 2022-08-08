fa937e3ce623cc5aa0d4bfbf30ec745d8ae807e4
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-08-08 20:37:36 +0300

    fix: expand store object

--- async-race/src/store/index.ts
+++ async-race/src/store/index.ts
@@ -1,7 +1,11 @@
 import { Store } from '../types';
+import { Numbers } from '../constants';
 
 const store: Store = {
   animationId: {},
+  winner: '',
+  hasWinner: false,
+  winnerTime: Numbers.zero,
 };
 
 export default store;
