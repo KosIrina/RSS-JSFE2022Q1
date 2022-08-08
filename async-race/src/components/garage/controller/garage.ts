611342a0226653e2e9fadabe2b2c17006fa53eb6
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-08-08 13:46:32 +0300

    feat: add methods to start and stop one car animation

--- async-race/src/components/garage/controller/garage.ts
+++ async-race/src/components/garage/controller/garage.ts
@@ -1,7 +1,10 @@
 import API from '../../../api';
 import CommonController from '../../common/controller/common';
 import CommonView from '../../common/view/common';
-import { COLOR, APP_TEXT_CONTENT } from '../../../constants';
+import { COLOR, APP_TEXT_CONTENT, Numbers, ENGINE_STATUS } from '../../../constants';
+import { IRaceParameters } from '../../../types';
+import Animation from '../../../utils/animation';
+import store from '../../../store';
 
 export default class GarageController {
   readonly api: API;
@@ -10,10 +13,13 @@ export default class GarageController {
 
   readonly appView: CommonView;
 
+  readonly animation: Animation;
+
   constructor() {
     this.api = new API();
     this.appController = new CommonController();
     this.appView = new CommonView();
+    this.animation = new Animation();
   }
 
   public editCar(name: string, color: string, id: number): void {
@@ -48,5 +54,105 @@ export default class GarageController {
     update.headingElement.innerHTML = `${
       this.appView.createViewName(APP_TEXT_CONTENT.garage, update.totalCars).innerHTML
     }`;
+  }
+
+  private updateButtonsStatesDuringRace(id: number): void {
+    const carContainer = document.querySelector(`.garage__car[data-car-id="${id}"]`) as HTMLElement;
+    (carContainer.querySelector('.garage__car-start-engine-button') as HTMLElement).setAttribute(
+      'disabled',
+      ''
+    );
+    (carContainer.querySelector('.garage__car-select-button') as HTMLElement).setAttribute(
+      'disabled',
+      ''
+    );
+    (carContainer.querySelector('.garage__car-remove-button') as HTMLElement).setAttribute(
+      'disabled',
+      ''
+    );
+    (document.querySelector('.car-creation__button') as HTMLElement).setAttribute('disabled', '');
+    (document.querySelector('.controller-buttons__race') as HTMLElement).setAttribute(
+      'disabled',
+      ''
+    );
+    (document.querySelector('.controller-buttons__generate-cars') as HTMLElement).setAttribute(
+      'disabled',
+      ''
+    );
+    (document.querySelector('.garage__previous-page-button') as HTMLElement).setAttribute(
+      'disabled',
+      ''
+    );
+    (document.querySelector('.garage__next-page-button') as HTMLElement).setAttribute(
+      'disabled',
+      ''
+    );
+  }
+
+  private updateButtonsStatesAfterStop(id: number): void {
+    const carContainer = document.querySelector(`.garage__car[data-car-id="${id}"]`) as HTMLElement;
+    (carContainer.querySelector('.garage__car-start-engine-button') as HTMLElement).removeAttribute(
+      'disabled'
+    );
+    (carContainer.querySelector('.garage__car-stop-engine-button') as HTMLElement).setAttribute(
+      'disabled',
+      ''
+    );
+    (carContainer.querySelector('.garage__car-select-button') as HTMLElement).removeAttribute(
+      'disabled'
+    );
+    (carContainer.querySelector('.garage__car-remove-button') as HTMLElement).removeAttribute(
+      'disabled'
+    );
+    (document.querySelector('.car-creation__button') as HTMLElement).removeAttribute('disabled');
+    (document.querySelector('.controller-buttons__race') as HTMLElement).removeAttribute(
+      'disabled'
+    );
+    (document.querySelector('.controller-buttons__generate-cars') as HTMLElement).removeAttribute(
+      'disabled'
+    );
+    this.appController.enableNextButton(APP_TEXT_CONTENT.garage);
+    this.appController.disableNextButton(APP_TEXT_CONTENT.garage);
+    this.appController.enablePreviousButton(APP_TEXT_CONTENT.garage);
+    this.appController.disablePreviousButton(APP_TEXT_CONTENT.garage);
+  }
+
+  public async activateStartCarButton(id: number): Promise<void> {
+    const { velocity, distance } = (await this.api.engine.operateEngine(
+      id,
+      ENGINE_STATUS.started
+    )) as IRaceParameters;
+    const time: number = Math.round(distance / velocity);
+
+    const carContainer = document.querySelector(`.garage__car[data-car-id="${id}"]`) as HTMLElement;
+    const car = carContainer.querySelector('.garage__car-image') as HTMLElement;
+    const flag = carContainer.querySelector('.garage__flag-image') as HTMLElement;
+    const distanceBetween = Math.floor(this.animation.getDistanceBetweenElements(car, flag));
+    const distanceBetweenPlusHalfCar = distanceBetween + Numbers.thirty;
+
+    this.updateButtonsStatesDuringRace(id);
+
+    this.animation.animateCar(car, distanceBetweenPlusHalfCar, time);
+
+    const { success } = (await this.api.engine.operateEngine(id, ENGINE_STATUS.drive)) as {
+      success: boolean;
+    };
+    if (!success) {
+      window.cancelAnimationFrame(+store.animationId[id]);
+    }
+
+    (carContainer.querySelector('.garage__car-stop-engine-button') as HTMLElement).removeAttribute(
+      'disabled'
+    );
+  }
+
+  public async activateStopCarButton(id: number): Promise<void> {
+    await this.api.engine.operateEngine(id, ENGINE_STATUS.stopped);
+
+    const carContainer = document.querySelector(`.garage__car[data-car-id="${id}"]`) as HTMLElement;
+    const car = carContainer.querySelector('.garage__car-image') as HTMLElement;
+    car.style.transform = `translateX(${Numbers.zero})`;
+
+    this.updateButtonsStatesAfterStop(id);
   }
 }
