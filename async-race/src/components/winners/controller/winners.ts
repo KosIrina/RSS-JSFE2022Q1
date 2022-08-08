bf3430c5340c9f7e2c715532b4a517f8413fde87
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-08-08 04:39:20 +0300

    fix: update method to delete winner when deleting car from garage

--- async-race/src/components/winners/controller/winners.ts
+++ async-race/src/components/winners/controller/winners.ts
@@ -17,15 +17,12 @@ export default class WinnersController {
   }
 
   public async removeCar(id: string): Promise<void> {
-    const carInWinners = document.querySelector(`.winners__row[data-car-id="${id}"]`);
-    if (carInWinners) {
-      const currentPage = this.appController.getCurrentPage(APP_TEXT_CONTENT.winners.toLowerCase());
-      await this.api.winners.deleteWinner(+id);
-      const winners = document.querySelector('.main__winners') as HTMLElement;
-      winners.innerHTML = '';
-      this.winners.drawMainWinners();
+    await this.api.winners.deleteWinner(+id);
+    const currentPage = this.appController.getCurrentPage(APP_TEXT_CONTENT.winners.toLowerCase());
+    const winners = document.querySelector('.main__winners') as HTMLElement;
+    winners.innerHTML = '';
+    this.winners.drawMainWinners();
 
-      await this.winners.drawWinnersContainer(currentPage);
-    }
+    await this.winners.drawWinnersContainer(currentPage);
   }
 }
